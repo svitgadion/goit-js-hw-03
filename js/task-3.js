e83d8e55e0eb222bea71bcdc9cb3b3d4560dee94
Svitlana Gadion <svitlanagadion@gmail.com>
2024-12-09 09:21:48 +0200

    add html and js

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,18 @@
 'use strict'
-function getElementWidth(content, padding, border) {
-    return (
-        Number.parseFloat(content) +
-        2 * Number.parseFloat(padding) +
-        2 * Number.parseFloat(border)
-    );
+function filterArray(numbers, value) {
+    const newArray = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            newArray.push(numbers[i]);
+        }
+    }
+
+    return newArray;
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
